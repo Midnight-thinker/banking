49dc3806672c832db1004193d03c56c616ab815d
Your Name <debasmitac73@gmail.com>
2022-06-06 20:06:30 +0530

    seventh change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Footer() {
   return (
     <div className='footer'>
-      <p className='text'>© Copyright Loans.Made with love by Debasmita Chakraborty</p>
+      <p className='text'>© Copyright Loans.Made by Debasmita Chakraborty</p>
       <a href='https://github.com/Midnight-thinker'><i class="footer-icons fa-brands fa-github"></i></a>
       <a href='https://www.instagram.com/__midnight__thinker__/'><i class="footer-icons fa-brands fa-instagram"></i></a>
       <a href='https://www.linkedin.com/in/debasmita-chakraborty-ba9773210/'><i class="footer-icons fa-brands fa-linkedin"></i></a>
